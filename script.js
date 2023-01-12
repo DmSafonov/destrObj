a8f3c01fb22607c968622d7c388e274fce6b5a30
DmSafonov <safonov-dmitry@list.ru>
2023-01-12 18:52:30 +0400

    com

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ const options = {
 };
 
 console.log(options.name);
+
+for (let key in options){ //перебор объекта
+   console.log(`Свойство ${key} объекта ${options[key]}`);
+};
+
+for (let key in options){ //перебор объекта в объекте
+
+   if(typeof(options[key]) === 'object'){
+      for(let i in options[key]){
+         console.log(`Свойство ${i} объекта ${options[key][i]}`);
+      }
+   } else{
+         console.log(`Свойство ${key} объекта ${options[key]}`);
+      }
+};
+
+const {border, bg} = options.colors; //Деструктуризация
+
+console.log(border);
